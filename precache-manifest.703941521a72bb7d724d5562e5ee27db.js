7cbefbdfd934b2b19364b4414ad4aa4d21961b78
Lia <liavibanez@gmail.com>
2020-09-08 21:07:23 -0500

    Updates

---
+++ precache-manifest.703941521a72bb7d724d5562e5ee27db.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337f68f8f5e3dbf84510976904852dff",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "c1d8089d3b988c4487e5",
+    "url": "/react_portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "de4e7ac169c132a9295f",
+    "url": "/react_portfolio/static/css/main.70a377a8.chunk.css"
+  },
+  {
+    "revision": "c1d8089d3b988c4487e5",
+    "url": "/react_portfolio/static/js/2.dfbb08b0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_portfolio/static/js/2.dfbb08b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4e7ac169c132a9295f",
+    "url": "/react_portfolio/static/js/main.69050b07.chunk.js"
+  },
+  {
+    "revision": "5397e927921dde71372c",
+    "url": "/react_portfolio/static/js/runtime-main.d05fa38a.js"
+  },
+  {
+    "revision": "995e49c273cf954f5c925d07d4861203",
+    "url": "/react_portfolio/static/media/burgertime.995e49c2.png"
+  },
+  {
+    "revision": "ea918e32a0bf55d3e5e7556ee9815a56",
+    "url": "/react_portfolio/static/media/camppic.ea918e32.jpg"
+  },
+  {
+    "revision": "03c979a56b6d94e6a7936f1219172316",
+    "url": "/react_portfolio/static/media/portfolio2.03c979a5.JPG"
+  },
+  {
+    "revision": "e3e025959456d31f09f288a8af296727",
+    "url": "/react_portfolio/static/media/web_organizer.e3e02595.png"
+  }
+]);
